33815e6b486251ff8f89926cfe170162568c4a44
Salome Gonzalez <alu0100507041@ull.edu.es>
2013-11-06 15:30:04 +0000

    Pasando test

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -26,4 +26,61 @@ class Matriz
     end
     return salida
   end
+
+  #Resta de Matrices
+  def -(other)
+    c=Matriz.new(@n,@m,[0,0],[0,0])
+    for i in 0...@n do
+      for j in 0...@m do
+        c.A[i][j] =@A[i][j] - other.A[i][j]
+      end
+    end
+   return c
+  end
+
+  #Suma de Matrices
+  def +(other) 
+    c=Matriz.new(@n,@m,[0,0],[0,0])
+    for i in 0...@n do
+      for j in 0...@m do
+        c.A[i][j] =@A[i][j] + other.A[i][j]
+      end
+    end
+    return c
+  end
+
+  #Multiplicacion de Matrices
+  def *(other)
+    c=Matriz.new(@n,@m,[0,0],[0,0])
+    for i in 0...@n do
+      for j in 0...@n do  
+        for k in 0...@n do
+          c.A[i][j] += @A[i][k] * other.A[k][j]           
+         end
+      end
+    end
+    c 
+  end
+
+  #Division de una matriz por un escalar
+  def /(other)
+    c=Matriz.new(@n,@m,[0,0],[0,0])
+    for i in 0...@n do
+      for j in 0...@m do
+        c.A[i][j] =@A[i][j] / other.to_f
+      end
+    end
+    return c
+  end
+
+  #Division de una matriz por un escalar
+  def mult(a)
+    c=Matriz.new(@n,@m,[0,0],[0,0])
+    for i in 0...@n do
+      for j in 0...@m do
+        c.A[i][j]=@A[i][j]*a
+      end
+    end
+    c
+  end
 end
